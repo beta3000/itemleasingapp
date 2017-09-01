897fcc7202fa572c62e384e362d7d7f7ef048590
Le Deng <z00382545@z00382545-mac.local>
2017-09-01 17:01:21 -0400

    add item table when adding listing

--- frontend/usergui/src/app/components/add-listing/add-listing.component.ts
+++ frontend/usergui/src/app/components/add-listing/add-listing.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ListingService } from '../../services/listing-service/listing.service';
+import { ItemService } from '../../services/item-service/item.service';
 import { AppConst } from '../../utils/app-const';
 import { Listing } from '../../models/listing';
+import {Http} from '@angular/http';
+import {Params, ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-listing',
@@ -13,9 +16,19 @@ export class AddListingComponent implements OnInit {
   private listingServerPath: string = AppConst.itemServerPath;
   private listing: Listing = new Listing();
 
+  rows = [];
+  selected = [];
+
   constructor(
-  	private listingService: ListingService
+  	private listingService: ListingService,
+    private itemService: ItemService,
+    private router:Router,
+    private http:Http,
+    private route:ActivatedRoute
   	) { 
+    this.fetch((data) => {
+      this.rows = data;
+    });
   }
 
   onAddListing() {
@@ -28,6 +41,55 @@ export class AddListingComponent implements OnInit {
   			console.log(error.text());
   		}
   	);
+  }
+
+  fetch(cb) {
+    this.itemService.findItemsByUser().subscribe(
+      res => {
+        console.log(res);
+        cb(res.json());
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  onSelect({ selected }) {
+    console.log('Select Event', selected, this.selected);
+
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
+
+  onUpdate(id) {
+    this.router.navigate(['/updateItem', id])
+  }
+
+  onDelete(id) {
+    this.itemService.deleteItemById(id).subscribe(
+      res => {
+        console.log(res);
+      }, 
+      error => {
+        console.log(error.text());
+      }
+    );
+  }
+
+  onActivate(event) {
+  }
+
+  add() {
+    this.selected.push(this.rows[1], this.rows[3]);
+  }
+
+  update() {
+    this.selected = [ this.rows[1], this.rows[3] ];
+  }
+
+  remove() {
+    this.selected = [];
   }
 
   ngOnInit() {}
