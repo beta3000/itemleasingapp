9cb799d6afbd97c1bc063b49ccf0ef48cfc0aed0
Le Deng <z00382545@z00382545-mac.local>
2017-09-05 21:44:02 -0400

    add eureka

--- userservice/src/main/java/com/itemleasing/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/itemleasing/userservice/UserserviceApplication.java
@@ -3,10 +3,12 @@ package com.itemleasing.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableEurekaClient
 @RefreshScope
 public class UserserviceApplication {
 
