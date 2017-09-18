2a588e2d87572f791df2dbb29bf77079e4ce4a19
Le Deng <z00382545@z00382545-mac.local>
2017-09-17 20:39:53 -0400

    add zuul filter

--- zuulserver/src/main/java/com/itemleasing/ZuulserverApplication.java
+++ zuulserver/src/main/java/com/itemleasing/ZuulserverApplication.java
@@ -1,14 +1,33 @@
 package com.itemleasing;
 
+import com.itemleasing.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 
 // Note: @EnableZuulServer is used when you want to build your own routing service
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulserverApplication {
+
+	public RestTemplate getRestTemplate() {
+		RestTemplate template = new RestTemplate();
+		List interceptors = template.getInterceptors();
+
+		if(interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulserverApplication.class, args);
